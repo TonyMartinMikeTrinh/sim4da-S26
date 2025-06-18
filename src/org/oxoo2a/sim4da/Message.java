57e6980e4ced81362e34a5395103d412c18905ab
Peter Sturm <sturm@uni-trier.de>
2025-06-18 11:49:01 +0200

    Comment for the copy() method added

--- src/org/oxoo2a/sim4da/Message.java
+++ src/org/oxoo2a/sim4da/Message.java
@@ -11,6 +11,14 @@ public abstract class Message {
     protected Message(Message original) {
     }
 
+    /**
+     * Creates a copy of this message.
+     * This method uses reflection to invoke the copy constructor of the subclass.
+     * Subclasses must define a proper copy constructor that takes an instance of the same class.
+     *
+     * @return a new instance of the same class with copied fields.
+     * @throws RuntimeException if the subclass does not define a proper copy constructor.
+     */
     public Message copy() {
         try {
             Constructor<? extends Message> ctor =
